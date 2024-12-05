32591d578fc40a38359b45bfadc7e0ae0e9422c3
Maicon Daniel <35271025+Maicondlp@users.noreply.github.com>
2024-12-05 18:53:16 -0300

    Add files via upload

---
+++ js/personalizado.js
@@ -0,0 +1,11 @@
+<script>
+    $(document).ready(function(){
+        $('nav a').on('click', function(e) {
+            e.preventDefault();
+            var target = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 800);
+        });
+    });
+</script>
